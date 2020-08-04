602088644b5547e2ad1b446af935b44f86f6b483
Peggy-liu <101808315@student.swin.edu.au>
2020-08-05 00:48:00 +1000

    opaque token introspection from seperate application

--- src/main/java/com/oauth2/example/config/AuthorizationServer.java
+++ src/main/java/com/oauth2/example/config/AuthorizationServer.java
@@ -8,6 +8,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 
 @Configuration
@@ -38,7 +39,11 @@ public class AuthorizationServer extends AuthorizationServerConfigurerAdapter {
 			.withClient("client3")
 			.secret("client3")
 			.scopes("read")
-			.authorizedGrantTypes("client_credentials");
+			.authorizedGrantTypes("client_credentials")
+		.and()
+			//resource server
+			.withClient("resource")
+			.secret("12345");
 	}
 
 	@Override
@@ -47,4 +52,10 @@ public class AuthorizationServer extends AuthorizationServerConfigurerAdapter {
 		endpoints.userDetailsService(inMemoryUserDetailsManager);
 	}
 
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.checkTokenAccess("isAuthenticated()");
+	}
+
+	
 }
